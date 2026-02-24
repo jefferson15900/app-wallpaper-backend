9375f90878566706c062184d1ba4c3996a4fed3a
Jefferson <jefferson@ejemplo.com>
2026-02-24 18:27:31 -0500

    Backend: solicitud de verificacion

--- src/models/Feedback.js
+++ src/models/Feedback.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const FeedbackSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    type: { type: String, enum: ['comentario', 'problema', 'reporte_wallpaper'], required: true },
+    type: { type: String, enum: ['comentario', 'problema', 'reporte_wallpaper','solicitud_verificacion'], required: true },
     message: { type: String, required: true },
     targetWallpaper: { type: mongoose.Schema.Types.ObjectId, ref: 'Wallpaper' }, 
     createdAt: { type: Date, default: Date.now }
